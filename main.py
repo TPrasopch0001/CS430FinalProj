5a66fe3794fc39ea02eaa739d5610f54191a29a1
Hashir <70011762+HashMaster02@users.noreply.github.com>
2024-04-26 17:54:00 -0500

    Progress thus far

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from typing import List, Tuple
 
 
 # itemID -> price
-price = {7: 2, 8: 5}
+# price = {7: 2, 8: 5}
 
 # All 2-tuples are read as: (itemID, quantity)
 # ((itemID, qty), ... ) -> price
-promos = {
+offers = {
+    (7, 1): 2,
+    (8, 1): 5,
     ((7, 3), (8, 2)): 10,
     (7, 3): 5,
 }
@@ -33,8 +35,30 @@ def get_plan(total_items: int, items: Tuple[Tuple[int, int], ...]):
         return cache[items]
 
     # calculate value of items
+    items_list = [list(item) for item in items]
+    total_cost = 0
+
+    for offer, cost in offers.items():
+        if isinstance(offer[0], int):
+            for pair in items_list:
+                if pair[0] == offer[0] and pair[1] > 0:
+                    pair[1] -= offer[1]
+                    items = tuple([tuple(items) for items in items_list])
+                    total_cost += cost + get_plan(total_items, items)
+        else:
+            for item in offer:
+                for pair in items_list:
+                    if pair[0] == item[0] and pair[1] > 0:
+                        pair[1] -= item[1]
+                        items = tuple([tuple(items) for items in items_list])
+                        total_cost += cost + get_plan(total_items, items)
+
+    cache[items] = total_cost
+    return cache[items]
 
 
-print(get_plan(2, ((7, 0), (8, 0))))
-print(get_plan(2, ((7, 3), (8, -1))))
-print(get_plan(2, ((7, 3), (8, 1))))
+# print(get_plan(2, ((7, 0), (8, 0))))
+# print(get_plan(2, ((7, 3), (8, -1))))
+# print(get_plan(2, ((7, 3), (8, 1))))
+# print(get_plan(2, ((7, 3), (8, 2))))
+print(get_plan(2, ((7, 1), (8, 1))))
